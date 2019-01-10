c33f5a81d87d069fde65037d45b5d3dcf8d8b944
ajeeshjoshy <jishcem@gmail.com>
2019-01-11 02:33:47 +0530

    set up angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from "./material.module";
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -21,10 +25,13 @@ import { TaskComponent } from './tasks/task.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     UsersModule,
     TasksModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent],
